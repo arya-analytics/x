1894daaa628bee70bcf5a62a7ced755919af07ff
emiliano bonilla <emilbon99@gmail.com>
2022-05-15 14:23:58 -0700

    NT added rand package

---
+++ rand/rand.go
@@ -0,0 +1,38 @@
+package rand
+
+import (
+	"math/rand"
+)
+
+func MapKey[K comparable, V any](m map[K]V) (key K) {
+	l := len(m)
+	if l == 0 {
+		return key
+	}
+	i, ri := 0, rand.Intn(l)
+	for k, _ := range m {
+		if i == ri {
+			key = k
+			break
+		}
+		i++
+	}
+	return key
+}
+
+func MapValue[K comparable, V any](m map[K]V) V {
+	return m[MapKey[K, V](m)]
+}
+
+func MapElem[K comparable, V any](m map[K]V) (K, V) {
+	k := MapKey[K, V](m)
+	return k, m[k]
+}
+
+func Elem[V any](options ...V) V {
+	return Slice[V](options)
+}
+
+func Slice[V any](slice []V) V {
+	return slice[rand.Intn(len(slice))]
+}
---
+++ rand/rand_test.go
@@ -0,0 +1,62 @@
+package rand_test
+
+import (
+	"github.com/arya-analytics/x/rand"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Rand", func() {
+	Describe("Map", func() {
+		var (
+			m map[int]int
+		)
+		BeforeEach(func() {
+			m = map[int]int{
+				1: 2,
+				3: 4,
+				5: 6,
+			}
+		})
+		Describe("Key", func() {
+			It("Should return a random key", func() {
+				key := rand.MapKey(m)
+				Expect(key > 0).To(BeTrue())
+				Expect(key < 6).To(BeTrue())
+			})
+			It("Should return the zero type when the map is empty", func() {
+				m = map[int]int{}
+				key := rand.MapKey(m)
+				Expect(key).To(BeZero())
+			})
+		})
+		Describe("Value", func() {
+			It("Should return a random value", func() {
+				value := rand.MapValue(m)
+				Expect(value > 0).To(BeTrue())
+				Expect(value <= 6).To(BeTrue())
+			})
+		})
+		Describe("Element", func() {
+			It("Should return a random element", func() {
+				key, value := rand.MapElem(m)
+				Expect(key > 0).To(BeTrue())
+				Expect(value <= 6).To(BeTrue())
+				Expect(key < 6).To(BeTrue())
+				Expect(value > 0).To(BeTrue())
+			})
+		})
+	})
+	Describe("Slice", func() {
+		It("Should return a random element in the slice", func() {
+			value := rand.Slice([]int{1, 2, 3, 4, 5, 6})
+			Expect(value > 0).To(BeTrue())
+		})
+	})
+	Describe("Element", func() {
+		It("Should return a random element", func() {
+			value := rand.Elem(1, 2, 3)
+			Expect(value > 0).To(BeTrue())
+		})
+	})
+})
