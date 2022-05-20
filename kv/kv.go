6fc4cd810b089ef46a430f6fb590ccadd503752f
emiliano bonilla <emilbon99@gmail.com>
2022-05-19 22:24:47 -0700

    DA-156 wrote layer 2 db interface

--- kv/kv.go
+++ kv/kv.go
@@ -2,12 +2,27 @@ package kv
 
 // |||||| ENGINE ||||||
 
-type KV interface {
-	Set(key []byte, value []byte) error
+// Reader is a readable key-value store.
+type Reader interface {
 	Get(key []byte) ([]byte, error)
-	Delete(key []byte) error
-	Close() error
 	IteratorEngine
+}
+
+// Writer is a writeable key-value store.
+type Writer interface {
+	Set(key []byte, value []byte) error
+	Delete(key []byte) error
+}
+
+// Closer is a key-value store that can be closed,
+// which blocks until all pending operations have persisted to disk.
+type Closer interface {
+	Close() error
+}
+
+type KV interface {
+	Reader
+	Close() error
 }
 
 type IteratorEngine interface {
