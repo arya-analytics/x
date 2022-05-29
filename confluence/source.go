84484f092f79d797c3623129dd3f5ee61335712c
emiliano bonilla <emilbon99@gmail.com>
2022-05-29 16:37:04 -0700

    DA-156 removed writer and wrote unary source'

--- confluence/source.go
+++ confluence/source.go
@@ -1,9 +1,5 @@
 package confluence
 
-import (
-	"github.com/arya-analytics/x/shutdown"
-)
-
 // Source is a segment that can send values to inlets
 type Source[V Value] interface {
 	OutTo(inlets ...Inlet[V])
@@ -20,31 +16,19 @@ func (s *CoreSource[V]) InFrom(_ ...Outlet[V]) { panic("sources cannot receive v
 
 func (s *CoreSource[V]) OutTo(inlets ...Inlet[V]) { s.Out = append(s.Out, inlets...) }
 
-// Writer implements the Segment interface that allows the caller to write values to it.
-type Writer[V Value] struct {
-	CoreSource[V]
-	Values chan V
+func (s *CoreSource[V]) Flow(ctx Context) {}
+
+type UnarySource[V Value] struct {
+	Out Inlet[V]
 }
 
-func (s *Writer[V]) Flow(ctx Context) {
-	ctx.Shutdown.Go(func(sig chan shutdown.Signal) error {
-		for v := range s.Values {
-			select {
-			case <-sig:
-				return nil
-			default:
-				for i, inlet := range s.Out {
-					if i == len(s.Out)-1 {
-						inlet.Inlet() <- v
-					}
-					select {
-					case inlet.Inlet() <- v:
-						break
-					default:
-					}
-				}
-			}
-		}
-		return nil
-	})
+func (u *UnarySource[V]) InFrom(_ ...Outlet[V]) { panic("sources cannot receive values") }
+
+func (u *UnarySource[V]) OutTo(inlets ...Inlet[V]) {
+	if len(inlets) != 1 {
+		panic("unary sources must have exactly one outlet")
+	}
+	u.Out = inlets[0]
 }
+
+func (u *UnarySource[V]) Flow(ctx Context) {}
