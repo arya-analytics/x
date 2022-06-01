44889fe59d4686b61c5df56d261406a412216380
emiliano bonilla <emilbon99@gmail.com>
2022-05-31 17:15:21 -0700

    DA-156 improvements to grpc interface

--- grpc/transport.go
+++ grpc/transport.go
@@ -5,26 +5,18 @@ import (
 	"github.com/arya-analytics/x/address"
 )
 
-type UnaryService[I, O any] interface {
-	Exec(ctx context.Context, req I) (res O, err error)
-}
-
-type UnaryClientTemplate[I, O any] interface {
-	Send(ctx context.Context, conn *ClientConn, req I) (res O, err error)
-}
-
 type UnaryClient[I, O any] struct {
-	pool     *Pool
-	Template UnaryClientTemplate[I, O]
+	Pool   *Pool
+	Sender func(ctx context.Context, conn *ClientConn, req I) (res O, err error)
 }
 
 func (t *UnaryClient[I, O]) Send(ctx context.Context, addr address.Address, req I) (res O, err error) {
-	conn, err := t.pool.Acquire(addr)
+	conn, err := t.Pool.Acquire(addr)
 	if err != nil {
 		return nil, err
 	}
 	defer conn.Release()
-	return t.Template.Send(ctx, conn, req)
+	return t.Sender(ctx, conn, req)
 }
 
 type UnaryServer[I, O any] struct {
