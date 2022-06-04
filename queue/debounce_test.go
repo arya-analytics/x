9251d3f6adbe483fe4fe959b07ec3f40b89568a5
emiliano bonilla <emilbon99@gmail.com>
2022-06-03 17:39:36 -0700

    DA-156 moved debounce queue to confluence API

--- queue/debounce_test.go
+++ queue/debounce_test.go
@@ -1,57 +1,46 @@
 package queue_test
 
 import (
+	"github.com/arya-analytics/x/confluence"
 	"github.com/arya-analytics/x/queue"
-	"github.com/arya-analytics/x/shutdown"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"go.uber.org/zap"
 	"time"
 )
 
 var _ = Describe("Debounce", func() {
 	var (
-		req chan []int
-		res chan []int
-		s   shutdown.Shutdown
+		req confluence.Stream[[]int]
+		res confluence.Stream[[]int]
 		d   *queue.Debounce[int]
+		ctx confluence.Context
 	)
 	BeforeEach(func() {
-		req = make(chan []int)
-		res = make(chan []int, 100)
-		s = shutdown.New()
 		d = &queue.Debounce[int]{
-			In:  req,
-			Out: res,
-			Config: queue.DebounceConfig{
-				Shutdown:  s,
-				Interval:  30 * time.Millisecond,
-				Threshold: 15,
-				Logger:    zap.NewNop(),
-			},
+			Interval:  30 * time.Millisecond,
+			Threshold: 15,
 		}
-		d.Start()
+		req = confluence.NewStream[[]int](10)
+		res = confluence.NewStream[[]int](10)
+		ctx = confluence.DefaultContext()
+		d.InFrom(req)
+		d.OutTo(res)
+		d.Flow(ctx)
 	})
 	It("Should flush the queue at a specified interval", func() {
-		req <- []int{1, 2, 3, 4, 5}
-		req <- []int{6, 7, 8, 9, 10}
+		req.Inlet() <- []int{1, 2, 3, 4, 5}
+		req.Inlet() <- []int{6, 7, 8, 9, 10}
 		time.Sleep(50 * time.Millisecond)
-		Expect(s.Shutdown()).To(Succeed())
-		var responses []int
-		for v := range res {
-			responses = append(responses, v...)
-		}
+		Expect(ctx.Shutdown.Shutdown()).To(Succeed())
+		responses := <-res.Outlet()
 		Expect(responses).To(Equal([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))
 	})
 	It("Should flush teh queue when the threshold is reached", func() {
-		req <- []int{1, 2, 3, 4, 5}
-		req <- []int{6, 7, 8, 9, 10}
-		req <- []int{11, 12, 13, 14, 15}
-		Expect(s.Shutdown()).To(Succeed())
-		var responses []int
-		for v := range res {
-			responses = append(responses, v...)
-		}
+		req.Inlet() <- []int{1, 2, 3, 4, 5}
+		req.Inlet() <- []int{6, 7, 8, 9, 10}
+		req.Inlet() <- []int{11, 12, 13, 14, 15}
+		Expect(ctx.Shutdown.Shutdown()).To(Succeed())
+		responses := <-res.Outlet()
 		Expect(responses).To(Equal([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}))
 	})
 })
