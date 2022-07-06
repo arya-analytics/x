3f85ca7f5225bdde080372b649ca9774e679f507
emiliano bonilla <emilbon99@gmail.com>
2022-07-06 22:01:39 +0200

    DA-169 ring queue initial commit

--- buffer/ring_queue.go
+++ buffer/ring_queue.go
@@ -1,24 +1,24 @@
-package queue
+package buffer
 
 const minQueueLen = 16
 
-// RingBuffer represents a single instance of the queue data structure.
-type RingBuffer[V any] struct {
+// RingQueue represents a single instance of the queue data structure.
+type RingQueue[V any] struct {
 	buf               []V
 	head, tail, count int
 }
 
-// NewRingBuffer constructs and returns a new RingBuffer.
-func NewRingBuffer[V any]() *RingBuffer[V] {
-	return &RingBuffer[V]{buf: make([]V, minQueueLen)}
+// NewRingBuffer constructs and returns a new RingQueue.
+func NewRingBuffer[V any]() *RingQueue[V] {
+	return &RingQueue[V]{buf: make([]V, minQueueLen)}
 }
 
 // Length returns the number of elements currently stored in the queue.
-func (q *RingBuffer[V]) Length() int { return q.count }
+func (q *RingQueue[V]) Length() int { return q.count }
 
 // resizes the queue to fit exactly twice its current contents
 // this can result in shrinking if the queue is less than half-full
-func (q *RingBuffer[V]) resize() {
+func (q *RingQueue[V]) resize() {
 	newBuf := make([]V, q.count<<1)
 
 	if q.tail > q.head {
@@ -34,7 +34,7 @@ func (q *RingBuffer[V]) resize() {
 }
 
 // Add puts an element on the end of the queue.
-func (q *RingBuffer[V]) Add(elem V) {
+func (q *RingQueue[V]) Add(elem V) {
 	if q.count == len(q.buf) {
 		q.resize()
 	}
@@ -47,7 +47,7 @@ func (q *RingBuffer[V]) Add(elem V) {
 
 // Peek returns the element at the head of the queue. This call panics
 // if the queue is empty.
-func (q *RingBuffer[V]) Peek() V {
+func (q *RingQueue[V]) Peek() V {
 	if q.count <= 0 {
 		panic("queue: Peek() called on empty queue")
 	}
@@ -58,7 +58,7 @@ func (q *RingBuffer[V]) Peek() V {
 // invalid, the call will panic. This method accepts both positive and
 // negative index values. Index 0 refers to the first element, and
 // index -1 refers to the last.
-func (q *RingBuffer[V]) Get(i int) interface{} {
+func (q *RingQueue[V]) Get(i int) interface{} {
 	// If indexing backwards, convert to positive index.
 	if i < 0 {
 		i += q.count
@@ -72,12 +72,12 @@ func (q *RingBuffer[V]) Get(i int) interface{} {
 
 // Remove removes and returns the element from the front of the queue. If the
 // queue is empty, the call will panic.
-func (q *RingBuffer[V]) Remove() V {
+func (q *RingQueue[V]) Remove() (v V) {
 	if q.count <= 0 {
 		panic("queue: Remove() called on empty queue")
 	}
 	ret := q.buf[q.head]
-	q.buf[q.head] = nil
+	q.buf[q.head] = v
 	// bitwise modulus
 	q.head = (q.head + 1) & (len(q.buf) - 1)
 	q.count--
