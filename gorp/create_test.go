0a1d11b9e20c970243a8424642902ca1c34e95ec
emiliano bonilla <emilbon99@gmail.com>
2022-07-18 12:13:56 +0200

    DA-182 added deletion test cases

--- gorp/create_test.go
+++ gorp/create_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/arya-analytics/x/gorp"
 	"github.com/arya-analytics/x/kv"
 	"github.com/arya-analytics/x/kv/memkv"
-	"github.com/cockroachdb/errors"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -30,28 +29,54 @@ var _ = Describe("Create", func() {
 	AfterEach(func() {
 		Expect(kvDB.Close()).To(Succeed())
 	})
-	It("Should create the entry in storage", func() {
-		var entries []entry
-		for i := 0; i < 10; i++ {
-			entries = append(entries, entry{ID: i, Data: "data"})
-		}
-		Expect(gorp.NewCreate[int, entry]().Entries(&entries).Exec(db)).To(Succeed())
-		var res []entry
-		Expect(gorp.NewRetrieve[int, entry]().Entries(&res).Exec(db)).To(Succeed())
-		Expect(res).To(Equal(entries))
+	Context("Single entry", func() {
+		It("Should create the entry in the DB", func() {
+			e := &entry{
+				ID:   42,
+				Data: "The answer to life, the universe, and everything",
+			}
+			Expect(gorp.NewCreate[int, entry]().Entry(e).Exec(db)).To(Succeed())
+			exists, err := gorp.NewRetrieve[int, entry]().WhereKeys(42).Exists(db)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(exists).To(BeTrue())
+		})
 	})
-	It("Should execute operations in a transaction", func() {
-		var entries []entry
-		txn := db.BeginTxn()
-		for i := 0; i < 10; i++ {
-			entries = append(entries, entry{ID: i, Data: "data"})
-		}
-		Expect(gorp.NewCreate[int, entry]().Entries(&entries).Exec(txn)).To(Succeed())
-		var res []entry
-		err := gorp.NewRetrieve[int, entry]().Entries(&res).Exec(db)
-		Expect(errors.Is(err, kv.NotFound))
-		Expect(txn.Commit()).To(Succeed())
-		Expect(gorp.NewRetrieve[int, entry]().Entries(&res).Exec(db)).To(Succeed())
-		Expect(res).To(Equal(entries))
+	Describe("Multiple entries", func() {
+		It("Should create the entries in the DB", func() {
+			var entries []entry
+			for i := 0; i < 10; i++ {
+				entries = append(entries, entry{ID: i, Data: "data"})
+			}
+			Expect(gorp.NewCreate[int, entry]().Entries(&entries).Exec(db)).To(Succeed())
+			var keys []int
+			for _, e := range entries {
+				keys = append(keys, e.ID)
+			}
+			exists, err := gorp.NewRetrieve[int, entry]().
+				WhereKeys(keys...).Exists(db)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(exists).To(BeTrue())
+		})
+	})
+	Describe("Txn", func() {
+		It("Should execute operations within a transaction", func() {
+			var (
+				entries []entry
+				keys    []int
+			)
+			txn := db.BeginTxn()
+			for i := 0; i < 10; i++ {
+				entries = append(entries, entry{ID: i, Data: "data"})
+				keys = append(keys, i)
+			}
+			Expect(gorp.NewCreate[int, entry]().Entries(&entries).Exec(txn)).To(Succeed())
+			exists, err := gorp.NewRetrieve[int, entry]().WhereKeys(keys...).Exists(db)
+			Expect(err).To(BeNil())
+			Expect(exists).To(BeFalse())
+			Expect(txn.Commit()).To(Succeed())
+			exists, err = gorp.NewRetrieve[int, entry]().WhereKeys(keys...).Exists(db)
+			Expect(err).To(BeNil())
+			Expect(exists).To(BeTrue())
+		})
 	})
 })
--- gorp/delete_test.go
+++ gorp/delete_test.go
@@ -1,12 +1,47 @@
 package gorp_test
 
 import (
+	"github.com/arya-analytics/x/gorp"
+	"github.com/arya-analytics/x/kv"
+	"github.com/arya-analytics/x/kv/memkv"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	"github.com/arya-analytics/x/gorp"
 )
 
 var _ = Describe("Delete", func() {
-
+	var (
+		db   *gorp.DB
+		kvDB kv.DB
+	)
+	BeforeEach(func() {
+		kvDB = memkv.Open()
+		db = gorp.Wrap(kvDB)
+	})
+	AfterEach(func() {
+		Expect(kvDB.Close()).To(Succeed())
+	})
+	Describe("WhereKeys", func() {
+		It("Should delete an entry by key in the DB", func() {
+			Expect(gorp.NewCreate[int, entry]().
+				Entry(&entry{ID: 1, Data: "Arya"}).
+				Exec(db)).To(Succeed())
+			Expect(gorp.NewDelete[int, entry]().WhereKeys(1).Exec(db)).To(Succeed())
+			exists, err := gorp.NewRetrieve[int, entry]().WhereKeys(1).Exists(db)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(exists).To(BeFalse())
+		})
+	})
+	Describe("Where", func() {
+		It("Should delete an entry by predicate in the DB", func() {
+			Expect(gorp.NewCreate[int, entry]().
+				Entry(&entry{ID: 1, Data: "Arya"}).
+				Exec(db)).To(Succeed())
+			Expect(gorp.NewDelete[int, entry]().Where(func(e *entry) bool {
+				return e.Data == "Arya"
+			}).Exec(db)).To(Succeed())
+			exists, err := gorp.NewRetrieve[int, entry]().WhereKeys(1).Exists(db)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(exists).To(BeFalse())
+		})
+	})
 })
